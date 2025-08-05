46d1f0c05d904ca2fa417eeada3e8fa96d96aaa5
TGP-17 <mytablet19048@gmail.com>
2025-08-05 21:39:23 +0200

    Update esync.c

--- server/esync.c
+++ server/esync.c
@@ -58,49 +58,52 @@ int do_esync(void)
 }
 
 static char shm_name[29];
+static char shm_path[256];
 static int shm_fd;
-static off_t shm_size;
-static void **shm_addrs;
-static int shm_addrs_size;  /* length of the allocated shm_addrs array */
-static long pagesize;
-
-static void shm_cleanup(void)
-{
-    close( shm_fd );
-    if (shm_unlink( shm_name ) == -1)
-        perror( "shm_unlink" );
-}
+static size_t pagesize;
+static size_t shm_size;
 
 void esync_init(void)
 {
     struct stat st;
 
-    if (fstat( config_dir_fd, &st ) == -1)
-        fatal_error( "cannot stat config dir\n" );
+    if (fstat(config_dir_fd, &st) == -1)
+        fatal_error("cannot stat config dir\n");
 
     if (st.st_ino != (unsigned long)st.st_ino)
-        sprintf( shm_name, "/wine-%lx%08lx-esync", (unsigned long)((unsigned long long)st.st_ino >> 32), (unsigned long)st.st_ino );
+        sprintf(shm_name, "/wine-%lx%08lx-esync",
+                (unsigned long)((unsigned long long)st.st_ino >> 32),
+                (unsigned long)st.st_ino);
     else
-        sprintf( shm_name, "/wine-%lx-esync", (unsigned long)st.st_ino );
+        sprintf(shm_name, "/wine-%lx-esync", (unsigned long)st.st_ino);
 
-    shm_unlink( shm_name );
+    snprintf(shm_path, sizeof(shm_path), "/data/data/com.termux/files/usr/tmp/%s", shm_name + 1);
 
-    shm_fd = shm_open( shm_name, O_RDWR | O_CREAT | O_EXCL, 0644 );
+    unlink(shm_path);
+
+    shm_fd = open(shm_path, O_RDWR | O_CREAT | O_EXCL, 0644);
     if (shm_fd == -1)
-        perror( "shm_open" );
+        perror("open");
 
-    pagesize = sysconf( _SC_PAGESIZE );
+    pagesize = sysconf(_SC_PAGESIZE);
 
-    shm_addrs = calloc( 128, sizeof(shm_addrs[0]) );
+    shm_addrs = calloc(128, sizeof(shm_addrs[0]));
     shm_addrs_size = 128;
 
     shm_size = pagesize;
-    if (ftruncate( shm_fd, shm_size ) == -1)
-        perror( "ftruncate" );
+    if (ftruncate(shm_fd, shm_size) == -1)
+        perror("ftruncate");
 
-    fprintf( stderr, "esync: up and running.\n" );
+    fprintf(stderr, "esync: up and running.\n");
 
-    atexit( shm_cleanup );
+    atexit(shm_cleanup);
+}
+
+static void shm_cleanup(void)
+{
+    close(shm_fd);
+    if (unlink(shm_path) == -1)
+        perror("unlink");
 }
 
 static struct list mutex_list = LIST_INIT(mutex_list);
